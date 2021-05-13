b8a54abbcc27ef424dd84d8981575ead14e299b7
Mayank pal <palmayank5494@gmail.com>
2021-05-13 21:59:26 +0530

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Header } from "./components/header.js";
+
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <Header/>
+    </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import "./header.css"
+
+export function Header(){
+    return(
+        <>
+        <div>
+            <h1>Refrrals India</h1>
+        </div>
+        <div>
+        <navbar>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+        </navbar>
+        </div>
+        </>)
+}
